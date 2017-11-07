dd6d03349af6f239e9a3bc27baca21cfc0ad34e2
Codrut Marin <codrutsoft13@gmail.com>
2017-11-07 15:55:48 +0100

    Add sh file

--- googlecloud.sh
+++ googlecloud.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yes | yum -y install nano;
 yes | yum -y install curl;
-echo -e "yes\n\nyes\nyes\n\n"| curl https://sdk.cloud.google.com | bash;
+echo -e "\nyes\nyes\n\n"| curl https://sdk.cloud.google.com | bash;
 exec -l $SHELL;
 yes | gcloud components install kubectl
