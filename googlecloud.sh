649ecf38c7b38a7cff6a6224ea545b2bf40229ac
Codrut Marin <codrutsoft13@gmail.com>
2017-11-07 16:43:22 +0100

    Add sh file for salt-minion

--- googlecloud.sh
+++ googlecloud.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yes | yum -y install nano;
 yes | yum -y install curl;
-echo -e "\nyes\nyes\n\n"| curl https://sdk.cloud.google.com | bash;
+echo "\nyes\nyes\n\n"| curl https://sdk.cloud.google.com | bash;
 exec -l $SHELL;
 yes | gcloud components install kubectl
---
+++ salt-minion.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+yes | yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
+yes | yum clear expire-cache
+yes | yum install salt-minion
+yes | yum install nano
+sed -i.back "s/^\(master:\).*/\110.200.155.85/" /etc/salt/minion
+sed -i "/^#.* master: /s/*#//" /etc/salt/minion
+sed -i.back "s/^\(hash_table:\).*/\1sha256/" /etc/salt/minion
+sed -i "/^#.* hash_type: /s/*#//" /etc/salt/minion
