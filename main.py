cd018e68b02d67fe77d6539a823a42d84ad52dc8
Codrut Marin <codrutsoft13@gmail.com>
2017-11-07 12:39:19 +0100

    Install gcloud, kubectl

--- main.py
+++ main.py
@@ -7,13 +7,13 @@
 # __utils__ = salt.loader.utils(__opts__)
 # __salt__ = salt.loader.minion_mods(__opts__, utils=__utils__)
 # print(__salt__["test.ping"]());
-
+#!./venv/bin/python
 import salt.client as client
 
 local = client.LocalClient();
 cmd = "yes | yum -y install nano;";
 cmd += "yes | yum -y install curl;";
-cmd += "yes | curl https://sdk.cloud.google.com | bash;";
+cmd += "yes 'y  y'| curl https://sdk.cloud.google.com | bash;";
 cmd += "exec -l $SHELL;";
 cmd += "yes | gcloud components install kubectl";
-print(local.cmd("*","cmd.run",[cmd]));
+print(local.cmd("*", "cmd.run", [cmd]));
