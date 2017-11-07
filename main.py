20151ab24169cab802fdad438e62a09ba788cfb0
Codrut Marin <codrutsoft13@gmail.com>
2017-11-07 11:42:58 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import salt.config as conf
+import salt.loader
+
+__opts__ = conf.minion_config("/etc/salt/minion")
+__grains__ = salt.loader.grains(__opts__)
+__opts__['grains'] = __grains__
+__utils__ = salt.loader.utils(__opts__)
+__salt__ = salt.loader.minion_mods(__opts__, utils=__utils__)
+__salt__["cmd.run"]["ifconfig"]();
