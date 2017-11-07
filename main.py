5783f730747a2d41addaf69bd9e9296a061279a6
Codrut Marin <codrutsoft13@gmail.com>
2017-11-07 11:51:49 +0100

    trying the salt.client

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import salt.config as conf
-import salt.loader
+# import salt.config as conf
+# import salt.loader
+#
+# __opts__ = conf.minion_config("/etc/salt/minion")
+# __grains__ = salt.loader.grains(__opts__)
+# __opts__['grains'] = __grains__
+# __utils__ = salt.loader.utils(__opts__)
+# __salt__ = salt.loader.minion_mods(__opts__, utils=__utils__)
+# print(__salt__["test.ping"]());
 
-__opts__ = conf.minion_config("/etc/salt/minion")
-__grains__ = salt.loader.grains(__opts__)
-__opts__['grains'] = __grains__
-__utils__ = salt.loader.utils(__opts__)
-__salt__ = salt.loader.minion_mods(__opts__, utils=__utils__)
-print(__salt__["test.ping"]());
+import salt.client as client
+
+local = client.LocalClient();
+local.cmd("*","cmd.run",["ifconfig"]);
