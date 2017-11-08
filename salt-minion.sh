c564437675333e7267a01ba77c3ebef91d2495b3
Codrut Marin <codrutsoft13@gmail.com>
2017-11-08 09:04:12 +0100

    Add minion conf file

--- salt-minion.sh
+++ salt-minion.sh
@@ -3,9 +3,6 @@ yes | yum -y install http://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7
 yes | yum -y clean expire-cache
 yes | yum -y install salt-minion
 yes | yum -y install nano
-#sed -i.back "s/^\(master:\).*/\110.200.155.85/" /etc/salt/minion
-#sed -i "/^#.* master: /s/*#//" /etc/salt/minion
-#sed -i.back "s/^\(hash_table:\).*/\1sha256/" /etc/salt/minion
-#sed -i "/^#.* hash_type: /s/*#//" /etc/salt/minion
-#systemctl start salt-minion.service
-#systemctl enable salt-minion.service
+cp ./minion /etc/salt/minion
+systemctl start salt-minion.service
+systemctl enable salt-minion.service
