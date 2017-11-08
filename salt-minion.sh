f0696cbb50c070ae6f67f1e6c4c307c6619cee20
Codrut Marin <codrutsoft13@gmail.com>
2017-11-08 08:49:34 +0100

    Add -y for unsupervise creating

--- salt-minion.sh
+++ salt-minion.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#yes | yum install http://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
-#yes | yum clean expire-cache
-#yes | yum install salt-minion
-#yes | yum install nano
+yes | yum -y install http://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
+yes | yum -y clean expire-cache
+yes | yum -y install salt-minion
+yes | yum -y install nano
 sed -i.back "s/^\(master:\).*/\110.200.155.85/" /etc/salt/minion
 sed -i "/^#.* master: /s/*#//" /etc/salt/minion
-#sed -i.back "s/^\(hash_table:\).*/\1sha256/" /etc/salt/minion
-#sed -i "/^#.* hash_type: /s/*#//" /etc/salt/minion
-#systemctl start salt-minion.service
-#systemctl enable salt-minion.service
+sed -i.back "s/^\(hash_table:\).*/\1sha256/" /etc/salt/minion
+sed -i "/^#.* hash_type: /s/*#//" /etc/salt/minion
+systemctl start salt-minion.service
+systemctl enable salt-minion.service
